96a0bbedf0b525a0e91d00d8b4190c841c4a8352
Alain Hélaïli <helaili@github.com>
2021-01-02 21:08:35 +0100

    ??

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ async function run() {
     const outputFilename = core.getInput('outputFilename')
     const token = core.getInput('token')
     console.log(token)
-    const octokit = github.getOctokit({
-      auth: token
-    })
+    const octokit = github.getOctokit(token)
 
     console.log(`Retrieving repositories for ${enterprise}!`)
     
