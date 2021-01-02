bdbb48721e7eb7989940b73325f790c552386a68
Alain Hélaïli <helaili@github.com>
2021-01-02 18:53:58 +0100

    get the octokit

--- index.js
+++ index.js
@@ -2,53 +2,52 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const fs = require('fs')
 
-try {
-  const enterprise = core.getInput('enterprise')
-  const outputFilename = core.getInput('outputFilename')
-  const token = core.getInput('token')
-  console.log(`Retrieving repositories for ${enterprise}!`)
-  
-  const query = `query($enterpriseName:String!, $cursor:String) {
-    enterprise(slug: $enterpriseName) {
-      name
-      organizations(first: 100, after: $cursor) {
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
-        totalCount
-        nodes {
-          name 
-          login
+async function run() {
+  try {
+    const enterprise = core.getInput('enterprise')
+    const outputFilename = core.getInput('outputFilename')
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+
+    console.log(`Retrieving repositories for ${enterprise}!`)
+    
+    const query = `query($enterpriseName:String!, $cursor:String) {
+      enterprise(slug: $enterpriseName) {
+        name
+        organizations(first: 10, after: $cursor) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          totalCount
+          nodes {
+            name 
+            login
+          }
         }
       }
+    }`;
+    const variables = {
+      enterpriseName: enterprise
     }
-  }`;
-  const variables = {
-    enterpriseName: enterprise
-  }
-  
-  const orgs = getOrgs(github, query, variables)
-
-  core.setOutput('repo-list', orgs)
-  if(outputFilename) {
-    fs.writeFileSync(outputFilename, JSON.stringify(orgs))
-  }
-} catch (error) {
-  core.setFailed(error.message);
-}
-
-function getOrgs(github, query, variables, orgs = []) {
-  console.log(github)
-  github.graphql(query, variables).then(results => {
-    const newOrgList = orgs.concat(result.enterprise.organizations.nodes)
-    const hasNextPage = result.enterprise.organizations.pageInfo.hasNextPage
     
-    if (hasNextPage) {
+    let hasNextPage = true
+    let orgs = []
+
+    while (hasNextPage) {
+      const result = await octokit.graphql(query, variables)
+      orgs = orgs.concat(result.enterprise.organizations.nodes)
+      hasNextPage = result.enterprise.organizations.pageInfo.hasNextPage
       variables.cursor = result.enterprise.organizations.pageInfo.endCursor
-      return getOrgs(github, query, variables, newOrgList)
-    } else {
-      return newOrgList
     }
-  })
+
+    core.setOutput('repo-list', orgs)
+    if(outputFilename) {
+      fs.writeFileSync(outputFilename, JSON.stringify(orgs))
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
